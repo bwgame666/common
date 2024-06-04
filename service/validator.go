4711736163df615675669b94563fb12c63accdf2
dev001 <dev001@gmail.com>
2024-06-04 10:04:38 +0800

    update

--- service/validator.go
+++ service/validator.go
@@ -3,10 +3,38 @@ package service
 import (
 	"fmt"
 	"github.com/bwgame666/common/libs"
+	"github.com/bytedance/sonic"
 	"github.com/go-playground/validator/v10"
 	"github.com/valyala/fasthttp"
 	"reflect"
 )
+
+func getRequestArgs(ctx *fasthttp.RequestCtx, paramValue interface{}) error {
+	if ctx.IsGet() || ctx.IsDelete() {
+		requestArg := ctx.QueryArgs()
+		fmt.Println("request args: ", requestArg)
+		requestMap := make(map[string]interface{})
+		requestArg.VisitAll(func(key, value []byte) {
+			requestMap[string(key)] = string(value)
+		})
+		jsonB, _ := sonic.Marshal(requestMap)
+		err := libs.JsonUnmarshal(jsonB, paramValue)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return err
+		}
+	} else if ctx.IsPost() || ctx.IsPut() {
+		// 读取post请求参数
+		requestBody := ctx.PostBody()
+		fmt.Println("Request body: ", requestBody)
+		err := libs.JsonUnmarshal(requestBody, paramValue)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return err
+		}
+	}
+	return nil
+}
 
 func validatorDecorator(svr *HttpService, handle RequestHandler) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
@@ -20,7 +48,7 @@ func validatorDecorator(svr *HttpService, handle RequestHandler) fasthttp.Reques
 		paramValue := reflect.New(paramType).Interface()
 
 		// 1、读取请求参数
-		err := libs.JsonUnmarshal([]byte(jsonStr), paramValue)
+		err := getRequestArgs(ctx, &paramValue)
 		if err != nil {
 			fmt.Println("Error:", err)
 			svr.response(ctx, data)
@@ -28,7 +56,7 @@ func validatorDecorator(svr *HttpService, handle RequestHandler) fasthttp.Reques
 		}
 
 		// 2、请求参数校验
-		req := reflect.ValueOf(reqPara).Elem().Interface()
+		req := reflect.ValueOf(paramValue).Elem().Interface()
 		validate := validator.New()
 		errV := validate.Struct(req)
 		if errV != nil {
