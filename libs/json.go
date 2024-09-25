02de5829c0f2cecbbf84342cd0a3c3830b61d81e
dev001 <dev001@gmail.com>
2024-09-25 11:00:38 +0800

    update

--- libs/json.go
+++ libs/json.go
@@ -1,21 +1,21 @@
 package libs
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/bytedance/sonic"
 )
 
 func JsonMarshal(v interface{}) ([]byte, error) {
-	return sonic.Marshal(v)
+	return json.Marshal(v)
 }
 
 func JsonUnmarshal(data []byte, v interface{}) error {
-	return sonic.Unmarshal(data, v)
+	return json.Unmarshal(data, v)
 }
 
 func JsonUnmarshalGeneral(data []byte) map[string]interface{} {
 	var ret map[string]interface{}
-	err := sonic.Unmarshal(data, &ret)
+	err := json.Unmarshal(data, &ret)
 	if err != nil {
 		fmt.Println("JsonUnmarshalGeneral failed: ", err)
 		return nil
